e852462636695dad02797d51f0f6fc91431cb86d
Christian Iversen <ci@iversenit.dk>
2016-09-23 22:09:43 +0200

    + Implemented formatter that supports client packets

--- __init__.py
+++ __init__.py
@@ -1,5 +1,37 @@
+from pprint import pprint
+
 def section(name):
     print(("[ %s ]" % name).center(80, "-"))
+
+def indent(lines, amount=4):
+#    indentation = ("|" + "." * (amount-1))
+    indentation = " " * amount
+    return [indentation + line for line in lines]
+
+def format_field(name, typ, cmt):
+    res = []
+    for line in cmt:
+        res.append("/// %s" % line)
+    res.append("%s: %s," % (name, typ))
+    return res
+
+def format_case(name, fields):
+    res = []
+    res.append("%s {" % name)
+    for index, field in enumerate(fields):
+        if index > 0:
+            res.append("")
+        res.extend(indent(format_field(*field)))
+    res.append("},")
+    return res
+
+def format_packet(name, cases):
+    res = []
+    for index, case in enumerate(cases):
+        if index > 0:
+            res.append("")
+        res.extend(format_case(*case))
+    return "\n".join(["pub enum %s {" % name] + indent(res) + ["}"])
 
 def generate(cortex, *args):
     assert len(args) == 1
@@ -7,20 +39,8 @@ def generate(cortex, *args):
 
     sections = cortex.organize(cortex.parse(open(template, "r")))
     enums, packets, structs = [], [], []
-    for typ, items in sections.items():
-        # print("Parsing [%s] section.." % typ)
-        if typ == "enum":
-            for header, lines, comment in items:
-                enums.append(cortex.parse_enum(header, lines))
-        elif typ == "packet":
-            for header, lines, comment in items:
-                print(cortex.parse_packet(header, lines))
-                packets.append(cortex.parse_packet(header, lines))
-        elif typ == "struct":
-            for header, lines, comment in items:
-                structs.append(cortex.parse_struct(header, lines))
-        elif typ == "flags":
-            pass
-        else:
-            print("Unknown section type [%s]" % typ)
-    # print(packets)
+    for header, lines, comment in sections.get("packet"):
+        packets.append(cortex.parse_packet(header, lines))
+
+    for packet in packets:
+        print(format_packet(*packet))
