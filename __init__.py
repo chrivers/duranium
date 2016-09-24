2c7e3b52738026718b86f5885464dd41c7e78896
Christian Iversen <ci@iversenit.dk>
2016-09-25 01:42:03 +0200

    + Implemented more complete rust formatter

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
+import fileinput
+import textwrap
 from pprint import pprint
 
 SAME_LINE_BRACES = True
+LINE_WIDTH = 80
 
 def section(name):
     print(("[ %s ]" % name).center(80, "-"))
@@ -11,12 +14,19 @@ def indent(lines, amount=4):
 
 def format_field(name, typ, cmt):
     res = []
-    for line in cmt:
-        res.append("/// %s" % line)
+    ind = "/// "
+    res.extend(
+        textwrap.wrap(
+            " ".join(cmt),
+            initial_indent=ind,
+            subsequent_indent=ind,
+            width=LINE_WIDTH
+        )
+    )
     res.append("%s: %s," % (name, typ))
     return res
 
-def format_case(name, fields):
+def format_case(name, fields, cmt):
     res = []
     if SAME_LINE_BRACES:
         res.append("%s {" % name)
@@ -30,22 +40,40 @@ def format_case(name, fields):
     res.append("},")
     return res
 
-def format_packet(name, cases):
+def format_packet(name, cases, cmt):
     res = []
     for index, case in enumerate(cases):
         if index > 0:
             res.append("")
         res.extend(format_case(*case))
-    return "\n".join(["pub enum %s {" % name] + indent(res) + ["}"])
+    return "\n".join(["pub enum %s {" % name] + indent(res) + ["}", ""])
+
+def format_enum(name, cases, cmt):
+    res = []
+    for index, case in enumerate(cases):
+        res.append("%-20s = 0x%08x," % (case[0], case[1]))
+    return "\n".join(["pub enum %s {" % name] + indent(res) + ["}", ""])
+
+def format_struct(name, cases, cmt):
+    res = []
+    for index, case in enumerate(cases):
+        res.append("%-20s: %s," % (case[0], case[1]))
+    return "\n".join(["pub struct %s {" % name] + indent(res) + ["}", ""])
 
 def generate(cortex, *args):
-    assert len(args) == 1
-    template = args[0]
+    sections = cortex.parse(fileinput.input(args))
 
-    sections = cortex.organize(cortex.parse(open(template, "r")))
-    enums, packets, structs = [], [], []
-    for header, lines, comment in sections.get("packet", []):
-        packets.append(cortex.parse_packet(header, lines))
+    formatters = {
+        "enum": format_enum,
+        "struct": format_struct,
+        "packet": format_packet,
+    }
 
-    for packet in packets:
-        print(format_packet(*packet))
+    for name, items in sections.items():
+        if name not in formatters:
+            print("Unknown type [%s]" % name)
+            continue
+        formatter = formatters[name]
+
+        for item in items:
+            print(formatter(*item))
