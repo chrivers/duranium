51dfbb8602b5620c2e37e8c6f73569ddf51c116a
Christian Iversen <ci@iversenit.dk>
2016-09-25 03:40:11 +0200

    + Added rust enums template example

---
+++ templates/enums.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code)]
+
+use num::ToPrimitive;
+
+macro_rules! enum_to_primitive {
+    ($name:ident) => {
+        impl ToPrimitive for $name {
+            fn to_i64(&self) -> Option<i64> {
+                return Some(*self as i64);
+            }
+
+            fn to_u64(&self) -> Option<u64> {
+                return Some(*self as u64);
+            }
+        }
+    }
+}
+
+% for enum in enums:
+enum_from_primitive! {
+    %if enum.name == "AudioMode":
+    #[derive(Debug,Clone,Copy,PartialEq)]
+    %elif enum.name == "ConsoleType":
+    #[derive(Debug,Clone,Copy,Eq,PartialEq,Hash)]
+    %else:
+    #[derive(Debug,Clone,Copy)]
+    %endif
+    %if enum.name == "ObjectType":
+    #[allow(non_camel_case_types)]
+    %endif    
+    pub enum ${enum.name}
+    {
+        % for case in enum.fields:
+        ${case.aligned_name} = ${case.aligned_hex_value},
+        % endfor
+    }
+}
+enum_to_primitive!(${enum.name});
+
+% endfor
