495a0a2c1f1579060661f51d6a74626032320529
Christian Iversen <ci@iversenit.dk>
2016-09-25 13:33:11 +0200

    * Converted struct fields into OrderedDicts

--- templates/server.rs
+++ templates/server.rs
@@ -4,20 +4,23 @@ pub mod writer;
 use ::packet::server::Ship;
 use ::packet::enums::*;
 
-<% packet = packets.get("ClientPacket") %>\
+<% packet = packets.get("ServerPacket") %>\
 #[derive(Debug)]
 pub enum ${packet.name}
 {
     % for name, case in packet.fields.items():
     ${case.name}
     {
-        % for field in case.fields.values():
-        % for line in format_comment(field.comment, indent="/// "):
+        % for index, field in enumerate(case.fields.values()):
+        % if index > 0:
+
+        % endif
+        % for line in format_comment(field.comment, indent="/// ", width=73):
         ${line}
         % endfor
         ${field.name}: ${field.type},
         % endfor
     },
-    
+
     % endfor
 }
---
+++ templates/structs.rs
@@ -0,0 +1,16 @@
+% for struct in structs.values():
+#[derive(Debug)]
+pub enum ${struct.name}
+{
+    % for index, field in enumerate(struct.fields.values()):
+    % if index > 0:
+
+    % endif
+    % for line in format_comment(field.comment, indent="/// ", width=73):
+    ${line}
+    % endfor
+    ${field.name}: ${field.type},
+    % endfor
+}
+
+% endfor
