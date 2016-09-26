35353cfdce49e6d84196159d455f9d77e8430b0f
Christian Iversen <ci@iversenit.dk>
2016-09-26 16:58:23 +0200

    + Added object type mapping (until we get better templates)

--- lib/rust.py
+++ lib/rust.py
@@ -38,3 +38,43 @@ def rust_type(tp):
         return "Object"
     else:
         raise TypeError("No type mapping defined for [%s]" % tp.name)
+
+def object_type(tp):
+    if not tp:
+        raise ValueError("Empty type")
+    primitive_map = {
+        "u8": "u8",
+        "u16": "u16",
+        "u32": "u32",
+        "u64": "u64",
+
+        "i8": "i8",
+        "i16": "i16",
+        "i32": "i32",
+
+        "f32": "f32",
+
+        "bool8": "bool8",
+        "bool16": "bool16",
+        "bool32": "bool32",
+
+        "string": "String",
+    }
+    if tp.name in primitive_map:
+        return primitive_map[tp.name]
+    elif tp.name == "sizedarray":
+        type = object_type(tp.target)
+        return "[%s; %d]" % (type, tp.arg)
+    elif tp.name in ("enum8", "enum32"):
+        if tp.target.name == "OrdnanceType":
+            return "[%s try %s]" % (tp.target.name, tp.name)
+        else:
+            return "[%s is %s]" % (tp.target.name, tp.name)
+    elif tp.name == "map" and tp.arg == "ShipSystem":
+        return "[%s; 8]" % tp.target.name
+    elif tp.name == "map" and tp.arg == "BeamFrequency":
+        return "[%s; 5]" % tp.target.name
+    elif tp.name == "bitflags":
+        return "[%s is bitflags_u32]" % tp.arg
+    else:
+        raise TypeError("No type mapping defined for [%s]" % tp.name)
